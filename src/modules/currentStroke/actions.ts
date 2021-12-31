e036212acbc78bbb415f1e654256363e66f07149
saifaldin14 <saifaldindeveloper@gmail.com>
2021-12-31 16:10:03 -0500

    Create actions.ts

---
+++ src/modules/currentStroke/actions.ts
@@ -0,0 +1,39 @@
+import { Point, Stroke } from "../../types";
+
+export const BEGIN_STROKE = "BEGIN_STROKE";
+export const UPDATE_STROKE = "UPDATE_STROKE";
+export const END_STROKE = "END_STROKE";
+export const SET_STROKE_COLOR = "SET_STROKE_COLOR";
+
+export type Action =
+  | {
+      type: typeof BEGIN_STROKE;
+      payload: Point;
+    }
+  | {
+      type: typeof UPDATE_STROKE;
+      payload: Point;
+    }
+  | {
+      type: typeof END_STROKE;
+    }
+  | {
+      type: typeof SET_STROKE_COLOR;
+      payload: string;
+    };
+
+export const beginStroke = (x: number, y: number) => {
+  return { type: BEGIN_STROKE, payload: { x, y } };
+};
+
+export const updateStroke = (x: number, y: number) => {
+  return { type: UPDATE_STROKE, payload: { x, y } };
+};
+
+export const endStroke = (historyLimit: number, stroke: Stroke) => {
+  return { type: END_STROKE, payload: { historyLimit, stroke } };
+};
+
+export const setStrokeColor = (color: string) => {
+  return { type: SET_STROKE_COLOR, payload: color };
+};
