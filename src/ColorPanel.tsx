6f3388c7262981c591dc6206e795bbb27e83b0df
saifaldin14 <saifaldindeveloper@gmail.com>
2021-12-31 12:58:39 -0500

    Update ColorPanel.tsx

--- src/ColorPanel.tsx
+++ src/ColorPanel.tsx
@@ -1,3 +1,26 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setStrokeColor } from "./actions";
+import COLORS from "./colors";
+
+export const ColorPanel = () => {
+  return (
+    <div className="window colors-panel">
+      <div className="title-bar">
+        <div className="title-bar-text">COLORS</div>
+      </div>
+      <div className="window-body colors">
+        {COLORS.map((color: string) => (
+          <div
+            key={color}
+            onClick={() => {
+              onColorChange(color);
+            }}
+            className="color"
+            style={{ backgroundColor: color }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
