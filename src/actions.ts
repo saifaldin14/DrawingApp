5d89abe05f57fc1230a3a6fe702d99372831f834
saifaldin14 <saifaldindeveloper@gmail.com>
2021-12-31 11:45:51 -0500

    Update actions.ts

--- src/actions.ts
+++ src/actions.ts
@@ -1,3 +1,19 @@
+import { Point } from "./types";
+
 export const BEGIN_STROKE = "BEGIN_STROKE";
 export const UPDATE_STROKE = "UPDATE_STROKE";
 export const END_STROKE = "END_STROKE";
+
+export type Action =
+  | {
+      type: typeof BEGIN_STROKE;
+      payload: Point;
+    }
+  | {
+      type: typeof UPDATE_STROKE;
+      payload: Point;
+    }
+  | {
+      type: typeof END_STROKE;
+      payload: Point;
+    };
