e5a3a31c2baf395a696e41cca23da2b50fdf1f36
saifaldin14 <saifaldindeveloper@gmail.com>
2021-12-31 12:09:17 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { beginStroke } from "./actions";
+import { currentStrokeSelector } from "./selectors";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  const startDrawing = () => {};
+  const currentStroke = useSelector(currentStrokeSelector);
+  const dispatch = useDispatch();
+  const startDrawing = ({
+    nativeEvent,
+  }: React.MouseEvent<HTMLCanvasElement>) => {
+    const { offsetX, offsetY } = nativeEvent;
+    dispatch(beginStroke(offsetX, offsetY));
+  };
   const endDrawing = () => {};
   const draw = () => {};
 
