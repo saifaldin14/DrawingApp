2eae8c69bcb158a2f73868e48b8dea424bb12faf
saifaldin14 <saifaldindeveloper@gmail.com>
2021-12-31 11:16:06 -0500

    Wrapped in provider

--- src/store.ts
+++ src/store.ts
@@ -2,4 +2,4 @@ import { rootReducer } from "./rootReducer";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import { createStore } from "redux";
 
-export const store = createStore(rootReducer, devToolsEnhancer("DrawingApp"));
+export const store = createStore(rootReducer, devToolsEnhancer());
