2221f30aa5080699e71aaa3f44e61cdccc6e7a80
saifaldin14 <saifaldindeveloper@gmail.com>
2021-12-31 17:03:01 -0500

    Used createReducer instead

--- src/modules/currentStroke/reducer.ts
+++ src/modules/currentStroke/reducer.ts
@@ -1,45 +1,17 @@
-import {
-  Action,
-  UPDATE_STROKE,
-  BEGIN_STROKE,
-  END_STROKE,
-  SET_STROKE_COLOR,
-} from "./actions";
+import { createReducer } from "@reduxjs/toolkit";
 import { RootState } from "../../types";
 
-const initialState: RootState["currentStroke"] = {
-  points: [],
-  color: "#000",
-};
-
-export const reducer = (
-  state: RootState["currentStroke"] = initialState,
-  action: Action
-) => {
-  switch (action.type) {
-    case BEGIN_STROKE: {
-      return {
-        ...state,
-        points: [action.payload],
-      };
-    }
-    case UPDATE_STROKE: {
-      return {
-        ...state,
-        points: [...state.points, action.payload],
-      };
-    }
-    case END_STROKE: {
-      return { ...state, points: [] };
-    }
-    case SET_STROKE_COLOR: {
-      return {
-        ...state,
-        color: action.payload,
-      };
-    }
-    default: {
-      return state;
-    }
-  }
-};
+export const reducer = createReducer(initialState, (builder) => {
+  builder.addCase(beginStroke, (state, action) => {
+    state.points = [action.payload];
+  });
+  builder.addCase(updateStroke, (state, action) => {
+    state.points.push(action.payload);
+  });
+  builder.addCase(setStrokeColor, (state, action) => {
+    state.color = action.payload;
+  });
+  builder.addCase(endStroke, (state, action) => {
+    state.points = [];
+  });
+});
--- src/modules/strokes/reducer.ts
+++ src/modules/strokes/reducer.ts
@@ -1,19 +1,14 @@
 import { RootState } from "../../types";
-import { Action, END_STROKE } from "./actions";
-
-export const reducer = (state: RootState["strokes"] = [], action: Action) => {
-  switch (action.type) {
-    case END_STROKE: {
-      const { historyLimit, stroke } = action.payload;
-
-      if (!stroke.points.length) {
-        return state;
-      }
-      return [...state.slice(0, state.length - historyLimit), stroke];
+import { createReducer } from "@reduxjs/toolkit";
+import { endStroke } from "../../actions";
+const initialStrokes: RootState["strokes"] = [];
+export const reducer = createReducer(initialStrokes, (builder) => {
+  builder.addCase(endStroke, (state, action) => {
+    const { historyIndex, stroke } = action.payload;
+    if (historyIndex === 0) {
+      state.push(stroke);
+    } else {
+      state.splice(-historyIndex, historyIndex, stroke);
     }
-
-    default: {
-      return state;
-    }
-  }
-};
+  });
+});
