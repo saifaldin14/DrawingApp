862018e12dc32e94dd46ad65ed6ea35653beda65
saifaldin14 <saifaldindeveloper@gmail.com>
2021-12-31 16:14:14 -0500

    Create reducer.ts

---
+++ src/modules/currentStroke/reducer.ts
@@ -0,0 +1,45 @@
+import {
+  Action,
+  UPDATE_STROKE,
+  BEGIN_STROKE,
+  END_STROKE,
+  SET_STROKE_COLOR,
+} from "./actions";
+import { RootState } from "../../types";
+
+const initialState: RootState["currentStroke"] = {
+  points: [],
+  color: "#000",
+};
+
+export const rootReducer = (
+  state: RootState["currentStroke"] = initialState,
+  action: Action
+) => {
+  switch (action.type) {
+    case BEGIN_STROKE: {
+      return {
+        ...state,
+        points: [action.payload],
+      };
+    }
+    case UPDATE_STROKE: {
+      return {
+        ...state,
+        points: [...state.points, action.payload],
+      };
+    }
+    case END_STROKE: {
+      return { ...state, points: [] };
+    }
+    case SET_STROKE_COLOR: {
+      return {
+        ...state,
+        color: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
